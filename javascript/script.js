c174f69e15d3de1b58db0cc55545410ccdf46df8
Script With Saad <muhammadsaad965000@gmail.com>
2024-10-01 20:37:02 +0500

    all files added at once

---
+++ javascript/script.js
@@ -0,0 +1,55 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const dateInput = document.querySelector("#date");
+//     const searchButton = document.querySelector(".input-box button");
+
+//     // Event listener to open the calendar when clicking on the input field
+//     dateInput.addEventListener("click", () => {
+//         dateInput.showPicker();
+//     });
+
+//     searchButton.addEventListener("click", () => {
+
+//     });
+// });
+
+
+let userInput = document.getElementById("date");
+userInput.max = new Date().toISOString().split("T")[0];
+
+
+function calculateAge() {
+    let birthDate = new Date(userInput.value);
+
+    let d1 = birthDate.getDate();
+    let m1 = birthDate.getMonth() + 1;
+    let y1 = birthDate.getFullYear();
+
+    let today = new Date;
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
+
+
+    let d3, m3, y3;
+
+    y3 = y2 - y1;
+
+    if (m2 >= m1) {
+        m3 = m2 - m1;
+    } else {
+        y3--;
+        m3 = 12 + m2 - m1;
+    }
+
+    if (d2 >= d1) {
+        d3 = d2 - d1;
+    } else {
+        m3--;
+        d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    }
+}
+
+function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate();
+}
